9f3dd43f5116d89cfbe357e948546551acf08ab5
Dan-Ant <daniele.antonacci@marconibari.edu.it>
2026-01-07 16:32:52 +0000

    consegna

--- main.cpp
+++ main.cpp
@@ -1,11 +1,42 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+void riempivettore(int v[], int w[], int n) {
+    int i;
+    int q[n];
+
+    i = 0;
+    while (i < n) {
+        if (q[i] % 2 == 0) {
+            v = q[i];
+        } else {
+            w = q[i];
+        }
+        i = i + 1;
+    }
 }
 
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
+int main() {
+
+    int n, i;
+
+    cout << "Quanti numeri?" << endl;
+    cin >> n;
+    int q[n];
+
+    i = 0;
+    while (i < n) {
+        q[i] = rand() % (n * 10);
+        i = i + 1;
+    }
+    riempivettore();
+    return 0;
+}
+
+
+
+
+
+
+
+
